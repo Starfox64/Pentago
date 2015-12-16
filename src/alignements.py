b81f1efa917129cee85b09ea7b7a2d005e49757c
_FR_Starfox64 <louisdijon21@yahoo.fr>
2015-12-16 20:05:20 +0100

    Fixed checkAlignH

--- src/alignements.py
+++ src/alignements.py
@@ -1,13 +1,13 @@
 def checkAlignH(n, grid, p, j):
-	if len(grid) == n and len(grid[0]) == n:
-		for line in grid:
-			aligned = 0
-			for val in line:
-				if val == j:
-					aligned += 1
-				else:
-					aligned = 0
+	for lineI in range(n):
+		line = grid[lineI]
+		aligned = 0
+		for i in range(n):
+			if line[i] == j:
+				aligned += 1
+			else:
+				aligned = 0
 
-				if aligned == p:
-					return True
+			if aligned == p:
+				return True
 	return False
