ea2edd4072b2862bd28871e8a500b18c6388a338
Eirheinger <orban.valentin@gmail.com>
2015-12-19 12:17:51 +0100

    Finished rotations.py

--- src/rotations.py
+++ src/rotations.py
@@ -1,6 +1,4 @@
-import copy
-
-def rotateSubGridR(m, l):
+def rotateR(m, l):
 	newList = []
 	for i in range(0, m):
 		annexList = []
@@ -10,19 +8,32 @@ def rotateSubGridR(m, l):
 	return newList
 
 
-def rotateSubGridL(m, l):
+def rotateL(m, l):
 	newList = l.copy()
 	for k in range(0, 3):
-		newList = rotateSubGridR(m, newList)
+		newList = rotateR(m, newList)
 	return newList
 
-def rotate(n, l, e, s):
-	pass
-	#en cours
 
+def rotate(n, l, e, left):
+	square = []
 
-lTest = [[1,4,7],[2,5,8],[3,6,9]]
-print(lTest)
-lTest = rotateSubGridL(len(lTest), lTest)
-print(lTest)
-rotate(len(lt),lt,1,True)
+	for i in range(0, n):
+		if i < n//2:
+			if e == 0:
+				square.append(l[i][0:n//2])
+			elif e == 1 :
+				square.append(l[i][n//2:n])
+		else:
+			if e == 2:
+				square.append(l[i][n//2:n])
+			elif e == 3:
+				square.append(l[i][0:n//2])
+
+	square = rotateL(n//2, square) if left else rotateR(n//2, square)
+	return square
+	#gerer le retour
+
+lTest = [[1, 2, 3, 4], [11, 22, 33, 44],
+		 [111, 222, 333, 444], [1111, 2222, 3333, 4444]]
+print(rotate(4, lTest, 1, True))
