f771c0c774d612cb5eb63c84ea1619f556303f5a
Eirheinger <orban.valentin@gmail.com>
2015-12-21 13:32:25 +0100

    rotations.py revised

--- src/rotations.py
+++ src/rotations.py
@@ -1,10 +1,12 @@
 def rotateR(m, l):
 	newList = []
-	for i in range(0, m):
+
+	for y in range(m - 1, -1, -1):
 		annexList = []
-		for j in range(m - 1, -1, -1):
-			annexList.append(l[j][i])
+		for x in range(0, m):
+			annexList.append(l[y][x])
 		newList.append(annexList.copy())
+
 	return newList
 
 
@@ -21,19 +23,21 @@ def rotate(n, l, e, left):
 	for i in range(0, n):
 		if i < n//2:
 			if e == 0:
-				square.append(l[i][0:n//2])
+				square.append(l[0:n//2][i])
 			elif e == 1 :
-				square.append(l[i][n//2:n])
+				square.append(l[n//2:n][i])
 		else:
 			if e == 2:
-				square.append(l[i][n//2:n])
+				square.append(l[n//2:n][i])
 			elif e == 3:
-				square.append(l[i][0:n//2])
+				square.append(l[0:n//2][i])
 
 	square = rotateL(n//2, square) if left else rotateR(n//2, square)
 	return square
 	#gerer le retour
 
-lTest = [[1, 2, 3, 4], [11, 22, 33, 44],
-		 [111, 222, 333, 444], [1111, 2222, 3333, 4444]]
-print(rotate(4, lTest, 1, True))
+lTest = [[1, 2, 3, 4],
+		 [11, 22, 33, 44],
+		 [111, 222, 333, 444],
+		 [1111, 2222, 3333, 4444]]
+print(rotate(4, lTest, 0, True))
