779ed04022226e5de49ed00c02be3560cb503c8d
_FR_Starfox64 <louisdijon21@yahoo.fr>
2016-01-13 22:16:20 +0100

    Implemented sound on chip placement and cleaned code

--- src/pentagoGUI.py
+++ src/pentagoGUI.py
@@ -1,39 +1,40 @@
 import pygame
 import os
-from src.rotations import *
+from random import randint
 from pygame.locals import *
 
 FRAME_SIZE = (640, 480)
+
+pygame.mixer.pre_init(44100, -16, 1, 512)
+pygame.mixer.init()
 pygame.init()
+
 mainFrame = pygame.display.set_mode(FRAME_SIZE)
 pygame.display.set_caption('Pentago')
 
-GRID = [
-	[1, 0, 0, 1, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 2, 0, 0, 2],
-	[1, 0, 0, 1, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 2, 0, 0, 2]
-]
 
 WD = os.getcwd()  # Working Directory
 
 # Resources #
-BG = pygame.image.load(os.path.join(WD, '..', 'resources', 'bg.jpg'))
+BG = pygame.image.load(os.path.join(WD, '../resources/bg.jpg'))
 BG = pygame.transform.scale(BG, FRAME_SIZE)
 mainFrame.blit(BG, (0, 0))
 
-pieceWhite = pygame.image.load(os.path.join(WD, '..', 'resources', 'pieceWhite.png')).convert_alpha()
+pieceWhite = pygame.image.load(os.path.join(WD, '../resources/pieceWhite.png')).convert_alpha()
 WHITE_CHIP = pygame.transform.scale(pieceWhite, (60, 60))
 
 EMPTY_CHIP = WHITE_CHIP.copy()
 EMPTY_CHIP.fill((255, 255, 255, 30), None, pygame.BLEND_RGBA_MULT)  # Makes chip transparent
 
-pieceBlack = pygame.image.load(os.path.join(WD, '..', 'resources', 'pieceBlack.png')).convert_alpha()
+pieceBlack = pygame.image.load(os.path.join(WD, '../resources/pieceBlack.png')).convert_alpha()
 BLACK_CHIP = pygame.transform.scale(pieceBlack, (60, 60))
 
 FONT = pygame.font.Font(None, 30)
+
+CHIP_SOUNDS = list()
+for i in range(1, 4):
+	CHIP_SOUNDS.append(pygame.mixer.Sound('../resources/chipsCollide' + str(i) + '.ogg'))
+
 
 # GUI Constants #
 BLOCK_SPACING = 30
@@ -50,12 +51,30 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
-BUTTONS = []
+GRID_SIZE = 6
 
 
 # Functions #
-def _initChipsPos_(grid):
+def generateGrid(n):
+	return [[0 for i in range(n)] for i in range(n)]
+
+
+def newGame():
+	global currentGrid, gameState, currentPlayer
+	currentGrid = generateGrid(GRID_SIZE)
+	gameState = 1
+	currentPlayer = 1
+
+	mainFrame.blit(BG, (0, 0))
+	drawGrid(currentGrid, mainFrame, True)
+	displayIndication(mainFrame, 'Place a ' + ('white' if currentPlayer == 1 else 'black') + ' chip.')
+
+
+def initClickPos(grid):
+	global clickPos
+	clickPos = list()
 	blocks = len(grid) // 3
+
 	for y in range(blocks):
 		yOffset = BLOCK_SPACING if y == 0 else BLOCK_SPACING * 2
 		for x in range(blocks):
@@ -68,105 +87,116 @@ def _initChipsPos_(grid):
 					xOffset2 = CHIP_SPACING + 30 if xx == 0 else CHIP_SPACING + 20
 					indX += xx
 					indY += yy
-#					print(posX + xx*xOffset2, posY + yy*yOffset2)
-					BUTTONS.append(
+
+					clickPos.append(
 						dict([
-							("name", "chip"),
-							("position", (posX + xx * xOffset2, posY + yy * yOffset2)),
-							("size", (CHIP_SIZE, CHIP_SIZE)),
-							("gamePhase", 1),
-							("listIndex", (indY, indX))
+							('type', 'slot'),
+							('pos', (posX + xx * xOffset2, posY + yy * yOffset2)),
+							('size', (CHIP_SIZE, CHIP_SIZE)),
+							('index', (indY, indX))
 						])
 					)
-_initChipsPos_(GRID)
 
 
-def drawBlock(grid, surface, posX, posY, indX, indY):
+#def drawBlock(grid, surface, posX, posY, indX, indY):
+#	surface.blit(BLOCK, (posX, posY))
+#	global clickPos
+#	for click in clickPos:
+#		if click['type'] == 'slot':
+#			if (indY <= click['index'][0] < indY + 3) and (indX <= click['index'][1] < indX + 3):
+#				chip = CHIPS[grid[click['index'][0]][click['index'][1]]]
+#				surface.blit(chip, (click['index'][0], click['index'][1]))
+
+
+def drawBlock(grid, surface, posX, posY, indX, indY, calcClickPos):
 	surface.blit(BLOCK, (posX, posY))
-	for z in BUTTONS:
-		if z["name"] == "chip":
-			if (indY <= z["listIndex"][0] < indY + 3) and (indX <= z["listIndex"][1] < indX + 3):
-				chip = CHIPS[  grid[z["listIndex"][0]][z["listIndex"][1] ]  ]
-				surface.blit(chip, (z["position"][0], z["position"][1]))
-#	for y in range(3):
-#		yOffset = CHIP_SPACING + 30 if y == 0 else CHIP_SPACING + 20
-#		for x in range(3):
-#			xOffset = CHIP_SPACING + 30 if x == 0 else CHIP_SPACING + 20
-#			chip = CHIPS[grid[indY + y][indX + x]]
-#			surface.blit(chip, (posX + x * xOffset, posY + y * yOffset))
+
+	for y in range(3):
+		yOffset = CHIP_SPACING + 10 if y == 0 else CHIP_SPACING + 20
+		for x in range(3):
+			xOffset = CHIP_SPACING + 10 if x == 0 else CHIP_SPACING + 20
+			chip = CHIPS[grid[indY + y][indX + x]]
+			surface.blit(chip, (posX + x * xOffset, posY + y * yOffset))
+
+			if calcClickPos:
+				global clickPos
+				clickPos.append(
+					dict([
+						('type', 'slot'),
+						('pos', (posX + x * xOffset, posY + y * yOffset)),
+						('size', (CHIP_SIZE, CHIP_SIZE)),
+						('index', (indY + y, indX + x))
+					])
+				)
 
 
-def drawGrid(grid, surface):
+def drawGrid(grid, surface, calcClickPos=False):
+	if calcClickPos:
+		global clickPos
+		clickPos = list()
+		print('Clearing')
+
 	blocks = len(grid) // 3
 	for y in range(blocks):
 		yOffset = BLOCK_SPACING if y == 0 else BLOCK_SPACING * 2
 		for x in range(blocks):
 			xOffset = BLOCK_SPACING if x == 0 else BLOCK_SPACING * 2
-			surface.blit(BLOCK, (x * BLOCK_SIZE[0] + xOffset, y * BLOCK_SIZE[1] + yOffset))
-			drawBlock(grid, surface, x * BLOCK_SIZE[0] + xOffset, y * BLOCK_SIZE[1] + yOffset, x * 3, y * 3)
+			drawBlock(grid, surface, x * BLOCK_SIZE[0] + xOffset, y * BLOCK_SIZE[1] + yOffset, x * 3, y * 3, calcClickPos)
 
 
-def putChip(player, chipPos):
+def playChipSound():
+	CHIP_SOUNDS[randint(0, 2)].play()
 
-	if GRID[chipPos[0]][chipPos[1]] == 0 :
-		GRID[chipPos[0]][chipPos[1]] = player
+
+def putChip(grid, player, chipPos):
+
+	if grid[chipPos[0]][chipPos[1]] == 0:
+		playChipSound()
+		grid[chipPos[0]][chipPos[1]] = player
 		return True
-	else:
-		return False
+	return False
 
 
-def displayIndication(surface, sentence, indicationLevel):
+def displayIndication(surface, sentence):
+	sentenceSurface = FONT.render(sentence, True, BLACK, WHITE)
+	sentenceRect = sentenceSurface.get_rect()
+	sentenceRect.topleft = (FRAME_SIZE[0] - FONT.size(sentence)[0], 0)
 
-	if indicationLevel == "instruction":
-		sentenceSurface = FONT.render(sentence, True, BLACK, WHITE)
-		sentenceRect = sentenceSurface.get_rect()
-		sentenceRect.topleft = (FRAME_SIZE[0] - FONT.size(sentence)[0], FRAME_SIZE[1]//2)
+	surface.blit(sentenceSurface, sentenceRect)
 
-	else:
-		sentenceSurface = FONT.render(sentence, True, RED, BLACK)
-		sentenceRect = sentenceSurface.get_rect()
-		sentenceRect.topleft = (FRAME_SIZE[0] - FONT.size(sentence)[0], FRAME_SIZE[1] - FONT.size(sentence)[1])
-
-	surface.blit(sentenceSurface,sentenceRect)
 
 # Event Loop #
 playing = True
 redraw = False
-currentPlayer = 1
-gameState = 1  # 1: Placing / 2: Rotating / 3: Over
-drawGrid(GRID, mainFrame)
-displayIndication(mainFrame, "Poser un pion " + ("blanc" if currentPlayer == 1 else "noir"), "instruction")
+newGame()
 
 while playing:
-
-
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			playing = False
+			break
 
 		if event.type == MOUSEBUTTONUP:
-			print(event.pos)
-			print(pygame.mouse.get_pos())
+			for click in clickPos:
+				if (
+					click['pos'][0] <= event.pos[0] <= (click['pos'][0] + click['size'][0]) and
+					click['pos'][1] <= event.pos[1] <= (click['pos'][1] + click['size'][1])
+				):
+					if click['type'] == 'slot':
+						if putChip(currentGrid, currentPlayer, click['index']):
+							currentPlayer = 2 if currentPlayer == 1 else 1
+							redraw = True
+					elif click['type'] == 'newgame':
+						newGame()
+					break
 
-			for i in BUTTONS:
-				if i["position"][0] <= event.pos[0] <= (i["position"][0] + i["size"][0]) and i["position"][1] <= event.pos[1] <= (i["position"][1] + i["size"][1]):
-					displayIndication(mainFrame, "Zone interactible", "warning")
-
-#					if gameState == i["gamePhase"]:
-#						if i["name"] == "chip":
-					if putChip(currentPlayer, i["listIndex"]) is True:
-						currentPlayer = 2 if currentPlayer == 1 else 1
-						redraw = True
-						break
-					else:
-						displayIndication(mainFrame, "Case Indisponible", "warning")
-						redraw = False
 	if redraw:
+		redraw = False
 		mainFrame.blit(BG, (0, 0))
-		drawGrid(GRID, mainFrame)
-		displayIndication(mainFrame, "Poser un pion " + ("blanc" if currentPlayer == 1 else "noir"), "instruction")
-
+		drawGrid(currentGrid, mainFrame)
+		displayIndication(mainFrame, 'Place a ' + ('white' if currentPlayer == 1 else 'black') + ' chip.')
 
 	pygame.display.update()
+
 
 pygame.quit()
