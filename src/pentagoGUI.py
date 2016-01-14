5184dc7a6b598303c245edf817b491d8db7ccd2d
_FR_Starfox64 <louisdijon21@yahoo.fr>
2016-01-14 16:09:19 +0100

    Finished game

--- src/pentagoGUI.py
+++ src/pentagoGUI.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import json
 from random import randint
 from pygame.locals import *
 
@@ -63,6 +64,7 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
 GRID_SIZE = 6
+ALIGN_WIN = 5
 
 
 # Functions #
@@ -70,18 +72,38 @@ def generateGrid(n):
 	return [[0 for i in range(n)] for i in range(n)]
 
 
-def newGame():
+def newGame(grid=None, state=1, player=1):
 	global currentGrid, gameState, currentPlayer
-	currentGrid = generateGrid(GRID_SIZE)
-	gameState = 1
-	currentPlayer = 1
+	currentGrid = grid or generateGrid(GRID_SIZE)
+	gameState = state
+	currentPlayer = player
 
 	mainFrame.blit(BG, (0, 0))
-	drawGrid(currentGrid, mainFrame, True)
-	displayIndication(mainFrame, 'Place a ' + ('white' if currentPlayer == 1 else 'black') + ' chip.')
+	draw(currentGrid, mainFrame, True)
+	drawText(mainFrame, 'Place a ' + ('white' if currentPlayer == 1 else 'black') + ' chip.',  BLACK, WHITE, 0)
+
+	pygame.display.update()
 
 
-def addClickPos(type, pos, size, args):
+def loadGame():
+	try:
+		file = open(os.path.join(WD, '../save.json'), 'r')
+	except FileNotFoundError:
+		return 'Save not available.'
+
+	gameData = json.loads(file.read())
+	file.close()
+	newGame(gameData['grid'], gameData['state'], gameData['player'])
+	return 'Game loaded.'
+
+
+def saveGame(grid, state, player):
+	file = open(os.path.join(WD, '../save.json'), 'w')
+	file.write(json.dumps({'grid': grid, 'state': state, 'player': player}))
+	return 'Game saved.'
+
+
+def addClickPos(type, pos, size, args=None):
 	clickPos.append({
 		'type': type,
 		'pos': pos,
@@ -96,8 +118,18 @@ def drawBlock(grid, surface, posX, posY, indX, indY, calcClickPos):
 	surface.blit(RIGHT_SLIDER if gameState == 2 else RIGHT_SLIDER_T, (posX - 5 + 4 * (BLOCK_SIZE[0] / 6), posY - 30))
 
 	if calcClickPos:
-		addClickPos('rotate', (posX - 5 + BLOCK_SIZE[0] / 6, posY - 30), (40, 30), {'left': True})  # BLOCKID
-		addClickPos('rotate', (posX - 5 + 4 * (BLOCK_SIZE[0] / 6), posY - 30), (40, 30), {'left': False})
+		addClickPos(
+			'rotate',
+			(posX - 5 + BLOCK_SIZE[0] / 6, posY - 30),
+			(40, 30),
+			{'left': True, 'indX': (indX + 1) // 3, 'indY': (indY + 1) // 3}
+		)
+		addClickPos(
+			'rotate',
+			(posX - 5 + 4 * (BLOCK_SIZE[0] / 6), posY - 30),
+			(40, 30),
+			{'left': False, 'indX': (indX + 1) // 3, 'indY': (indY + 1) // 3}
+		)
 
 	for y in range(3):
 		yOffset = CHIP_SPACING + 10 if y == 0 else CHIP_SPACING + 20
@@ -115,14 +147,21 @@ def drawBlock(grid, surface, posX, posY, indX, indY, calcClickPos):
 				)
 
 
-def drawGrid(grid, surface, calcClickPos=False):
+def draw(grid, surface, calcClickPos=False):
+	newRect = drawText(surface, ' New Game', WHITE, BLACK, 50)
+	loadRect = drawText(surface, 'Load Game', WHITE, BLACK, 75)
+	saveRect = drawText(surface, 'Save Game', WHITE, BLACK, 100)
+
 	if calcClickPos:
 		global clickPos
 		clickPos = list()
+		addClickPos('newgame', newRect.topleft, newRect.size)
+		addClickPos('loadgame', loadRect.topleft, loadRect.size)
+		addClickPos('savegame', saveRect.topleft, saveRect.size)
 
 	blocks = len(grid) // 3
 	for y in range(blocks):
-		yOffset = BLOCK_SPACING if y == 0 else BLOCK_SPACING * 2
+		yOffset = 20 + (BLOCK_SPACING if y == 0 else BLOCK_SPACING * 2)
 		for x in range(blocks):
 			xOffset = BLOCK_SPACING if x == 0 else BLOCK_SPACING * 2
 			drawBlock(grid, surface, x * BLOCK_SIZE[0] + xOffset, y * BLOCK_SIZE[1] + yOffset, x * 3, y * 3, calcClickPos)
@@ -141,17 +180,153 @@ def putChip(grid, player, chipPos):
 	return False
 
 
-def displayIndication(surface, sentence):
-	sentenceSurface = FONT.render(sentence, True, BLACK, WHITE)
-	sentenceRect = sentenceSurface.get_rect()
-	sentenceRect.topleft = (FRAME_SIZE[0] - FONT.size(sentence)[0], 0)
+def drawText(surface, text, textCol=BLACK, bgCol=WHITE, y=None):
+	textSurface = FONT.render(text, True, textCol, bgCol)
+	rect = textSurface.get_rect()
+	if y is None:  # Bottom Right
+		rect.bottomright = (FRAME_SIZE[0], FRAME_SIZE[1])
+	else:
+		rect.topright = (FRAME_SIZE[0], y)
 
-	surface.blit(sentenceSurface, sentenceRect)
+	surface.blit(textSurface, rect)
+	return rect
+
+
+def rotateR(m, l):
+	newList = []
+
+	for col in range(m):
+		annexList = []
+		for line in range(m - 1, -1, -1):
+			annexList.append(l[line][col])
+		newList.append(annexList.copy())
+
+	return newList
+
+
+def rotateL(m, l):
+	for k in range(3):
+		l = rotateR(m, l)
+	return l
+
+
+def rotate(n, l, indX, indY, left):
+	square = []
+
+	# isoler le quadrant n. sqr
+	if indX == 0 and indY == 0:
+		for line in range(n//2):
+			linedSquare = []
+			for col in range(n//2):
+				linedSquare.append(l[line][col])
+			square.append(linedSquare.copy())
+	elif indX == 0 and indY == 1:
+		for line in range(n//2, n):
+			linedSquare = []
+			for col in range(n//2):
+				linedSquare.append(l[line][col])
+			square.append(linedSquare.copy())
+	elif indX == 1 and indY == 1:
+		for line in range(n//2, n):
+			linedSquare = []
+			for col in range(n//2, n):
+				linedSquare.append(l[line][col])
+			square.append(linedSquare.copy())
+	elif indX == 1 and indY == 0:
+		for line in range(n//2):
+			linedSquare = []
+			for col in range(n//2, n):
+				linedSquare.append(l[line][col])
+			square.append(linedSquare.copy())
+
+	# effectuer la rotation du quadrant
+	square = rotateL(n//2, square) if left else rotateR(n//2, square)
+
+	# reinserer le quadrant dans la liste
+	if indX == 0 and indY == 0:
+		for line in range(n//2):
+			for col in range(n//2):
+				l[line][col] = square[line][col]
+	elif indX == 0 and indY == 1:
+		for line in range(n//2, n):
+			for col in range(n//2):
+				l[line][col] = square[line - 3][col]
+	elif indX == 1 and indY == 1:
+		for line in range(n//2, n):
+			for col in range(n//2, n):
+				l[line][col] = square[line - 3][col - 3]
+	elif indX == 1 and indY == 0:
+		for line in range(n//2):
+			for col in range(n//2, n):
+				l[line][col] = square[line][col - 3]
+
+	return l
+
+
+def checkAlignH(n, grid, p, j):
+	for lineI in range(n):
+		aligned = 0
+		for colI in range(n):
+			if grid[lineI][colI] == j:
+				aligned += 1
+			else:
+				aligned = 0
+
+			if aligned == p:
+				return True
+	return False
+
+
+def checkAlignV(n, grid, p, j):
+	for colI in range(n):
+		aligned = 0
+		for lineI in range(n):
+			if grid[lineI][colI] == j:
+				aligned += 1
+			else:
+				aligned = 0
+
+			if aligned == p:
+				return True
+	return False
+
+
+def checkAlignDBT(n, grid, p, j):
+	for lineI in range(n - 1, -1, -1):
+		for colI in range(n):
+			if grid[lineI][colI] == j:
+				offset = 1
+				while (lineI - offset >= 0) and (colI + offset < n) and (grid[lineI - offset][colI + offset] == j):
+					offset += 1
+					if offset == p:
+						return True
+	return False
+
+
+def checkAlignDTB(n, grid, p, j):
+	for lineI in range(n):
+		for colI in range(n):
+			if grid[lineI][colI] == j:
+				offset = 1
+				while (lineI + offset < n) and (colI + offset < n) and (grid[lineI + offset][colI + offset] == j):
+					offset += 1
+					if offset == p:
+						return True
+	return False
+
+
+def checkAlign(n, grid, p, j):
+	return (
+		checkAlignH(n, grid, p, j) or
+		checkAlignV(n, grid, p, j) or
+		checkAlignDBT(n, grid, p, j) or
+		checkAlignDTB(n, grid, p, j)
+	)
 
 
 # Event Loop #
 playing = True
-redraw = False
+update = False
 newGame()
 
 while playing:
@@ -166,24 +341,50 @@ while playing:
 					click['pos'][0] <= event.pos[0] <= (click['pos'][0] + click['size'][0]) and
 					click['pos'][1] <= event.pos[1] <= (click['pos'][1] + click['size'][1])
 				):
+					update = True
+					bottomNotif = None
+
 					if click['type'] == 'slot' and gameState == 1:
 						if putChip(currentGrid, currentPlayer, click['args']['index']):
-							currentPlayer = 2 if currentPlayer == 1 else 1
+							if checkAlign(len(currentGrid), currentGrid, ALIGN_WIN, currentPlayer):
+								gameState = 3
+								bottomNotif = 'The ' + ('whites' if currentPlayer == 1 else 'blacks') + ' won!'
+								break
 							gameState = 2
-							redraw = True
 					elif click['type'] == 'rotate' and gameState == 2:
-						print('Rotated')
+						currentGrid = rotate(
+							len(currentGrid),
+							currentGrid,
+							click['args']['indX'],
+							click['args']['indY'],
+							click['args']['left']
+						)
+						if checkAlign(len(currentGrid), currentGrid, ALIGN_WIN, currentPlayer):
+							gameState = 3
+							bottomNotif = 'The ' + ('whites' if currentPlayer == 1 else 'blacks') + ' won!'
+							break
+						currentPlayer = 2 if currentPlayer == 1 else 1
+						gameState = 1
 					elif click['type'] == 'newgame':
 						newGame()
+					elif click['type'] == 'savegame':
+						bottomNotif = saveGame(currentGrid, gameState, currentPlayer)
+					elif click['type'] == 'loadgame':
+						bottomNotif = loadGame()
 					break
 
-	if redraw:
-		redraw = False
+	if update:
+		update = False
 		mainFrame.blit(BG, (0, 0))
-		drawGrid(currentGrid, mainFrame)
-		displayIndication(mainFrame, 'Place a ' + ('white' if currentPlayer == 1 else 'black') + ' chip.')
+		draw(currentGrid, mainFrame)
+		if gameState == 1:
+			drawText(mainFrame, 'Place a ' + ('white' if currentPlayer == 1 else 'black') + ' chip.', BLACK, WHITE, 0)
+		elif gameState == 2:
+			drawText(mainFrame, 'Now rotate a block.', BLACK, WHITE, 0)
+		if bottomNotif is not None:
+			drawText(mainFrame, bottomNotif)
 
-	pygame.display.update()
+		pygame.display.update()
 
 
 pygame.quit()
