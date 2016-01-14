5134801a9223e70d838015233f099f3e37a45350
Eirheinger <orban.valentin@gmail.com>
2016-01-14 14:49:59 +0100

    rotate(grid_length, grid, indX, indY, left) -indX of block -indY of block -left = true if left rotation, false otherwise

--- src/rotations.py
+++ src/rotations.py
@@ -12,33 +12,32 @@ def rotateR(m, l):
 def rotateL(m, l):
 	for k in range(3):
 		l = rotateR(m, l)
-
 	return l
 
 
-def rotate(n, l, sqr, left):
+def rotate(n, l, indX, indY, left):
 	square = []
-	#isoler le quadrant n. sqr
 
-	if sqr == 0:
+	#isoler le quadrant n. sqr
+	if indX == 0 and indY == 0:
 		for line in range(n//2):
 			linedSquare = []
 			for col in range(n//2):
 				linedSquare.append(l[line][col])
 			square.append(linedSquare.copy())
-	elif sqr == 1:
+	elif indX == 0 and indY == 1:
 		for line in range(n//2, n):
 			linedSquare = []
 			for col in range(n//2):
 				linedSquare.append(l[line][col])
 			square.append(linedSquare.copy())
-	elif sqr == 2:
+	elif indX == 1 and indY == 1:
 		for line in range(n//2, n):
 			linedSquare = []
 			for col in range(n//2, n):
 				linedSquare.append(l[line][col])
 			square.append(linedSquare.copy())
-	elif sqr == 3:
+	elif indX == 1 and indY == 0:
 		for line in range(n//2):
 			linedSquare = []
 			for col in range(n//2, n):
@@ -49,34 +48,21 @@ def rotate(n, l, sqr, left):
 	square = rotateL(n//2, square) if left else rotateR(n//2, square)
 
 	#reinserer le quadrant dans la liste
-	if sqr == 0:
+	if indX == 0 and indY == 0:
 		for line in range(n//2):
 			for col in range(n//2):
 				l[line][col] = square[line][col]
-	elif sqr == 1:
+	elif indX == 0 and indY == 1:
 		for line in range(n//2, n):
 			for col in range(n//2):
 				l[line][col] = square[line - 3][col]
-	elif sqr == 2:
+	elif indX == 1 and indY == 1:
 		for line in range(n//2, n):
 			for col in range(n//2, n):
 				l[line][col] = square[line - 3][col - 3]
-	elif sqr == 3:
+	elif indX == 1 and indY == 0:
 		for line in range(n//2):
 			for col in range(n//2, n):
 				l[line][col] = square[line][col - 3]
 
 	return l
-
-
-lTest = [
-	[1, 0, 0, 1, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 2, 0, 0, 2],
-	[1, 0, 0, 1, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 2, 0, 0, 2]
-]
-result = rotate(6, lTest, 0, False)
-for i in result:
-	print(i)
